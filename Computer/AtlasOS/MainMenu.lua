a33712196d58e8c46f879b42a783f7714a0b4419
Atlasim1 <70275601+Atlasim1@users.noreply.github.com>
2023-06-16 15:57:45 -0400

    updated

--- Computer/AtlasOS/MainMenu.lua
+++ Computer/AtlasOS/MainMenu.lua
@@ -4,4 +4,4 @@ term.setCursorPos(1,1)
 print("AtlasOS 1.0 Logged in as ", _CURRENT_USER)
 while true do
     print("Open Program (Type shell for shell)")
-    shell.run(io.read())
+    shell.run("fg" .. io.read())
