2906e33848b9408b5d62f587c11cd32a136525ae
Atlasim1 <70275601+Atlasim1@users.noreply.github.com>
2023-06-16 16:04:31 -0400

    fixed issues

--- Computer/AtlasOS/MainMenu.lua
+++ Computer/AtlasOS/MainMenu.lua
@@ -1,7 +1,8 @@
 term.setTextColor(colors.yellow)
 term.clear()
 term.setCursorPos(1,1)
-print("AtlasOS 1.0 Logged in as ", _CURRENT_USER)
+print("AtlasOS 1.0")
+term.setTextColor(colors.white)
 while true do
     print("Open Program (Type shell for shell)")
     shell.run("fg" .. io.read())
