175ce6019cb2c8290902e3c9f972a6ec437fe0e2
Atlasim1 <70275601+Atlasim1@users.noreply.github.com>
2023-06-16 20:15:45 -0400

    Update MainMenu.lua

--- Computer/AtlasOS/MainMenu.lua
+++ Computer/AtlasOS/MainMenu.lua
@@ -5,5 +5,5 @@ print("AtlasOS 1.0")
 term.setTextColor(colors.white)
 while true do
     print("Open Program (Type shell for shell)")
-    shell.run("fg" .. io.read())
+    shell.run("fg " .. io.read())
 end
